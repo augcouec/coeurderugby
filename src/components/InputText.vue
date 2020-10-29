66c048b0a28505dfb3d886afb709c03756f87479
Antoine Chassagne <antoine.chassagne@me.com>
2020-10-30 00:32:50 +0100

    add inputs components

---
+++ src/components/InputText.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="field-container">
+    <label :for="name">{{ label }}</label>
+    <input :name="name" :placeholder="placeholder" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputText",
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    placeholder: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.field-container {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 50px;
+
+  label {
+    margin-bottom: 10px;
+    text-transform: uppercase;
+  }
+
+  input {
+    background-color: #f4f4f4;
+    border-radius: 12px;
+    border: solid 1px transparent;
+    padding: 13px 20px;
+    color: black;
+
+    &::placeholder {
+      font-style: italic;
+      color: #939393;
+    }
+  }
+}
+</style>

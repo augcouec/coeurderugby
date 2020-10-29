41dd03163d12a2cd6f4066f9295657ee82f8c407
Antoine Chassagne <antoine.chassagne@me.com>
2020-10-29 10:43:26 +0100

    Add styles base

---
+++ src/components/PaniniCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="yeah">{{ person.firstName }} {{ person.lastName }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PaniniCard",
+  props: {
+    person: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss"></style>
--- src/pages/Carotte.vue
+++ src/pages/Carotte.vue
@@ -1,10 +1,31 @@
 <template>
-  <h1>Album donateurs</h1>
+  <main>
+    <h1>Album donateurs</h1>
+    <PaniniCard
+      v-for="(person, index) in persons"
+      :key="index"
+      :person="person"
+    />
+  </main>
 </template>
 
 <script>
+import PaniniCard from "@/components/PaniniCard";
+
 export default {
   name: "CarottePage",
+  components: {
+    PaniniCard,
+  },
+  data() {
+    return {
+      persons: [
+        { firstName: "John", lastName: "Doe" },
+        { firstName: "Jane", lastName: "Doe" },
+        { firstName: "Jack", lastName: "Doe" },
+      ],
+    };
+  },
 };
 </script>
 

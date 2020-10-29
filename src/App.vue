3f0478a3d34a2d134806a7c436cb834a91070293
Antoine Chassagne <antoine.chassagne@me.com>
2020-10-29 15:06:06 +0100

    add fonts

--- src/App.vue
+++ src/App.vue
@@ -20,5 +20,29 @@ export default {
 </script>
 
 <style lang="scss">
+@font-face {
+  font-family: "Gotham";
+  src: url("./assets/fonts/GothamBook.ttf");
+  font-weight: normal;
+}
+
+@font-face {
+  font-family: "Gotham";
+  src: url("./assets/fonts/Gotham-Black.otf");
+  font-weight: bold;
+}
+
+@font-face {
+  font-family: "Poppins";
+  src: url("./assets/fonts/Poppins-Regular.ttf");
+  font-weight: normal;
+}
+
+@font-face {
+  font-family: "Poppins";
+  src: url("./assets/fonts/Poppins-Bold.ttf");
+  font-weight: bold;
+}
+
 @import "@/styles/main.scss";
 </style>
